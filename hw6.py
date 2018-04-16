14f4b054c5dcf615b77147410c6136c0aae8bec7
andrewipark <andrewipark@users.noreply.github.com>
2018-04-16 18:49:26 -0500

    Add HW6 stub

---
+++ hw6.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+# CS 196 SP18 HW6
+# This is a pretty short homework set.
+
+def factorial(the_num):
+    """
+    Returns the factorial of a number. This is the product of all integers between 1 and the number, inclusive.
+    
+    Examples:
+        Input:
+            the_num = 5
+        Output:
+            120
+            5 factorial (5!) = 5*4*3*2*1
+
+    Args:
+        (int) the_num: input number
+
+    Returns:
+        (int) the factorial of the_num
+    """
+    pass
+
+def fibonacci(n):
+    """
+    Returns the nth fibonacci number.
+    
+    The Fibonacci sequence is defined recursively. The 0th and 1st Fibonacci numbers are 0 and 1,
+    and the nth Fibonacci number is the sum of the (n-1)th and (n-2)th Fibonacci numbers.
+    
+    Args:
+        (int) n: Which fibonacci number to find
+
+    Returns:
+        (int) the nth Fibonacci number
+    """
+    pass
+
+def longest_increasing_subsequence(arr):
+    """
+    Given a list of numbers, returns the length of the longest increasing subsequence.
+    
+    A subsequence is an ordered but not necessarily contiguous part of an array.
+    
+    Examples:
+        Input:
+            [1,5,2,3]
+        Output:
+            3
+            The longest increasing subsequence is [1,2,3].
+            For this array:
+            [1,5] is also an increasing subsequence.
+            [1,2,3,5] is increasing, but not a subsequence.
+            [5,3] is a subsequence, but not increasing.
+            [3,2] is neither increasing nor a subsequence.
+    
+    Args:
+        ([int]) arr: The input list
+
+    Returns:
+        (int) the length of the longest increasing subsequence
+    
+    """
+    pass
+
